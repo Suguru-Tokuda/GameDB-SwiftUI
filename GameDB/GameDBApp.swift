22921b361d2c2e84bcd35665f4c0022865e80ad2
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-07 14:40:56 -0500

    Initial Commit

---
+++ GameDB/GameDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameDBApp.swift
+//  GameDB
+//
+//  Created by Suguru Tokuda on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GameDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
